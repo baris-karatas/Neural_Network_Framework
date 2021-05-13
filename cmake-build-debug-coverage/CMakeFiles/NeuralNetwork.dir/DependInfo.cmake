826cfa673dbac49477795957a06fc91fcbb22531
Barış Karataş <33378290+baris-karatas@users.noreply.github.com>
2021-05-14 01:00:14 +0300

    Add files via upload

---
+++ cmake-build-debug-coverage/CMakeFiles/NeuralNetwork.dir/DependInfo.cmake
@@ -0,0 +1,88 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/blackstone/Desktop/NeuralNetwork/Layer.cpp" "/home/blackstone/Desktop/NeuralNetwork/cmake-build-debug-coverage/CMakeFiles/NeuralNetwork.dir/Layer.cpp.o"
+  "/home/blackstone/Desktop/NeuralNetwork/Network.cpp" "/home/blackstone/Desktop/NeuralNetwork/cmake-build-debug-coverage/CMakeFiles/NeuralNetwork.dir/Network.cpp.o"
+  "/home/blackstone/Desktop/NeuralNetwork/Output.cpp" "/home/blackstone/Desktop/NeuralNetwork/cmake-build-debug-coverage/CMakeFiles/NeuralNetwork.dir/Output.cpp.o"
+  "/home/blackstone/Desktop/NeuralNetwork/main.cpp" "/home/blackstone/Desktop/NeuralNetwork/cmake-build-debug-coverage/CMakeFiles/NeuralNetwork.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/blackstone/Downloads/opencv-4.5.1/build"
+  "/home/blackstone/Downloads/opencv-4.5.1/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudev/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/core/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudaarithm/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/flann/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/imgproc/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/intensity_transform/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/ml/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/phase_unwrapping/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/plot/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/quality/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/reg/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/surface_matching/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudafilters/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudaimgproc/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudawarping/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/dnn/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/dnn_superres/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/features2d/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/freetype/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/fuzzy/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/hfs/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/img_hash/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/imgcodecs/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/line_descriptor/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/photo/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/saliency/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/text/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/videoio/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/xphoto/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/calib3d/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudafeatures2d/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudastereo/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/datasets/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/highgui/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/mcc/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/objdetect/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/rapid/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/rgbd/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/shape/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/structured_light/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/ts/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/video/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/xfeatures2d/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/ximgproc/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/xobjdetect/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/aruco/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/bgsegm/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/bioinspired/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/ccalib/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudabgsegm/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudalegacy/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudaobjdetect/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/dnn_objdetect/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/dpm/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/face/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/gapi/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/optflow/include"
+  "/home/blackstone/Downloads/opencv-4.5.1/modules/stitching/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/tracking/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/cudaoptflow/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/stereo/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/superres/include"
+  "/home/blackstone/Downloads/opencv_contrib-4.5.1/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
